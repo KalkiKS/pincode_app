608ecbcf36bcc427d30cedad45da10c96413fbbf
Kalki KS <peeterp10098@gmail.com>
2026-02-10 19:11:42 +0530

    Add verification and config missing validation in database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,22 @@
 from pymongo import MongoClient
+from pymongo.database import Database
+from typing import Optional
 from app.core.config import MONGO_URI, DATABASE_NAME
 
 class MongoDB:
-    client: MongoClient = None # type: ignore
-    db = None
+    client: Optional[MongoClient] = None
+    db: Optional[Database] = None
 
 mongodb = MongoDB()
 
 def connect_to_mongo():
+    if not MONGO_URI or not DATABASE_NAME:
+        raise RuntimeError("MongoDB config missing")
     mongodb.client = MongoClient(MONGO_URI)
-    mongodb.db = mongodb.client[DATABASE_NAME] # type: ignore
+    mongodb.db = mongodb.client[DATABASE_NAME]
     print("MongoDB connected")
 
 def close_mongo_connection():
-    mongodb.client.close()
-    print("MongoDB disconnected")
+    if mongodb.client:
+        mongodb.client.close()
+        print("MongoDB disconnected")
